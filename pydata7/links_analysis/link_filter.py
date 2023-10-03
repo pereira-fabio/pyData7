9a359d71df7b1eff714ac9e452407582298635ee
pereira-fabio <fabio.pereira.002@student.uni.lu>
2023-10-03 20:18:24 +0200

    added comments

--- pydata7/links_analysis/link_filter.py
+++ pydata7/links_analysis/link_filter.py
@@ -29,15 +29,23 @@ def get_unique_cve_id(path_to_json):
 
 
 def data_filtering():
+
     for regex in regex_data:
+        # A list to store the complied regex
         list_regex = []
+        # Iterate through the regex patterns and compile them
         for regex in regex["regexps"]:
+            # Compile the regex from platform.json
             complied_regex = re.compile(regex["regexp"])
             list_regex.append(complied_regex)
+        # Iterate through the data and check if the url matches the regex
         for item in data:
+            # Iterate through the list of complied regex
             for regex in list_regex:
+                # Check if the url matches the regex
                 if regex.search(item["url"]):
                     filtered_data.append(item)
+    # Write the filtered data to a json file
     with open("filtered_data.json", "w") as filtered_file:
         json.dump(filtered_data, filtered_file)
     return filtered_data
