54f331c9c4493f61912507f306c9a61041774728
pereira-fabio <fabio.pereira.002@student.uni.lu>
2023-10-24 18:15:22 +0200

    removed useless code, and added new a way to only get the commits and also the organization with the project name. Will change to only one function

--- pydata7/links_analysis/valid_link.py
+++ pydata7/links_analysis/valid_link.py
@@ -10,69 +10,52 @@ from pydata7.links_analysis.link_filter import data_filtering
 save_path = "../data/"
 
 # Path to the data from the data_generator
-path_to_json = data_filtering()  # "../data/filtered_data_2023-10-04_15-09-07.json"
-
-# A Dictionary to store filtered data which only contains the 200 status code
-valid_data = []
-# The delay interval between requests
-delay_interval = 3.6
+path_to_json = data_filtering()
 
 with open(path_to_json, "r") as f:
     data = json.load(f)
 
 
-async def valid_link(item, session):
-    url = item.get("url")
-    # Checks if the url is not empty
-    if url:
-        # In case of a timeout or a connection error, the program will exit
-        try:
-            async with session.head(url) as response:
-                # Checks if the response is 200 as it is the only valid response
-                if response.status == 200:
-                    # Adds the item to the valid_data list
-                    valid_data.append(item)
-                    print(len(valid_data), "links were validated")
-                else:
-                    # print how many are not valid
-                    print(f"{response.status} is not valid")
-        except aiohttp.ClientError as err:
-            print(f"An error occurred while checking the URL {url}: {err}")
-        # Waits for the delay interval
-        print(f"Waiting for {delay_interval} seconds")
-        await asyncio.sleep(delay_interval)
-    else:
-        print("No URL found")
+def has_commit():
+    contains_commit = []
+    for item in data:
+        if "commit" in item["url"]:
+            contains_commit.append(item)
+    return json_file_generation(contains_commit, "contains_commit")
 
 
-async def process_data():
-    async with aiohttp.ClientSession() as session:
-        tasks = []
-        for item in data:
-            task = valid_link(item, session)
-            tasks.append(task)
-        await asyncio.gather(*tasks)
-        print(len(valid_data), "links were validated")
-        await json_file_generation()
+def get_organization_project():
+    with open(has_commit(), "r") as f:
+        commit_json = json.load(f)
+
+    org_proj = []
+    for item in commit_json:
+        # Get the organization and project name from the url
+        organization_project = item["url"].split("/")[3:5]
+        # Add the organization and project name to the dictionary
+        item["organization_project"] = organization_project
+        # Add the dictionary to the list
+        org_proj.append(item)
+    return json_file_generation(org_proj, "org_proj")
 
 
-async def json_file_generation():
+def json_file_generation(array,name):
     # Gets the current date and time as a string. Year - Month - Day _ Hour - Minute - Second
     current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
     # Saves the data into a json file with the current date and time
-    file_name = f"valid_data_{current_time}.json"
+    file_name = f"{name}_{current_time}.json"
 
     # Creates the path to save the json file
     file_path = os.path.join(save_path, file_name)
 
     # Writes the data into a json file
     with open(file_path, "w") as json_file:
-        json.dump(valid_data, json_file)
+        json.dump(array, json_file)
 
     # returns the path to the json file, so it can be used in the next function
     return file_path
 
 
 if __name__ == "__main__":
-    asyncio.run(process_data())
+    get_organization_project()
--- pydata7/scripts/create_db.py
+++ pydata7/scripts/create_db.py
@@ -12,10 +12,14 @@ mydb = myclient["pydata7"]  # database
 # Creating collections
 filtered_collection = mydb["filtered"]
 unfiltered_collection = mydb["unfiltered"]
+commits_collection = mydb["commits"]
+org_proj_collection = mydb["org_proj"]
 
 # Path to the data (will change)
-path_to_filtered = "../data/filtered_data_2023-10-04_15-09-07.json"
-path_to_unfiltered = "../data/data_2023-10-03_13-52-43.json"
+path_to_filtered = "../data/filtered_data_2023-10-24_17-32-15.json"
+path_to_unfiltered = "../data/raw_data_2023-10-24_17-31-20.json"
+path_to_valid = "../data/contains_commit_2023-10-24_17-59-03.json"
+path_to_org_proj = "../data/org_proj_2023-10-24_17-59-03.json"
 
 # the data is inserted only if the collection is empty (only for testing purposes)
 # Inserting data into the database
@@ -26,5 +30,13 @@ if filtered_collection.count_documents({}) == 0:
 if unfiltered_collection.count_documents({}) == 0:
     with open(path_to_unfiltered, "r") as f:
         unfiltered_collection.insert_many(eval(f.read()))
+
+if commits_collection.count_documents({}) == 0:
+    with open(path_to_valid, "r") as f:
+        commits_collection.insert_many(eval(f.read()))
+
+if org_proj_collection.count_documents({}) == 0:
+    with open(path_to_org_proj, "r") as f:
+        org_proj_collection.insert_many(eval(f.read()))
 
 print(filtered_collection.find_one())
