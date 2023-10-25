1b814d49e16a51ebac2a5fdb4e5c504abd2ee8d2
pereira-fabio <fabio.pereira.002@student.uni.lu>
2023-10-25 12:55:04 +0200

    made the data import more dynamic, where it goes in the data file and adds it to the database

--- pydata7/scripts/create_db.py
+++ pydata7/scripts/create_db.py
@@ -1,42 +1,43 @@
 from pymongo import MongoClient
+import os
 
 # I can only connect to a database which is not running on docker container
 # Connecting to the database
 myclient = MongoClient("mongodb://localhost:27017/")
-mydb = myclient["pydata7"]  # database
+mydb = myclient["pydata7"]  # database name
 
-# Dropping collections if they exist
-#mydb.drop_collection("filtered")
-#mydb.drop_collection("unfiltered")
+path_to_data = "../data/"
 
-# Creating collections
-filtered_collection = mydb["filtered"]
-unfiltered_collection = mydb["unfiltered"]
-commits_collection = mydb["commits"]
-org_proj_collection = mydb["org_proj"]
 
-# Path to the data (will change)
-path_to_filtered = "../data/filtered_data_2023-10-24_17-32-15.json"
-path_to_unfiltered = "../data/raw_data_2023-10-24_17-31-20.json"
-path_to_valid = "../data/contains_commit_2023-10-24_17-59-03.json"
-path_to_org_proj = "../data/org_proj_2023-10-24_17-59-03.json"
+# Imports data into the database by going through the data folder
+# Since any data might be needed in the future, it makes sense to import all the data
+def import_data():
+    # Dropping collections if they exist (might remove it, since we want to update the data and not drop it)
+    if len(mydb.list_collection_names()) != 0:
+        print("The database is not empty")
+        myclient.drop_database("pydata7")
+        print("The database was dropped")
+    # Checking if the database is empty (the condition might change as well)
+    if len(mydb.list_collection_names()) == 0:
+        print("The database is empty")
+        # Creating collections
+        for filename in os.listdir(path_to_data):
+            if filename.endswith(".json"):
+                # Splits the name of the file and gets the first two parts
+                parts = filename.split("_")[0:2]
+                # Joins the first two parts and creates a collection name
+                collection_name = parts[0] + "_" + parts[1]
+                # Creates a collection
+                collection = mydb["".join(collection_name)]
+                insert_data(path_to_data + filename, collection)
+                print(collection_name, "was inserted into the database")
 
-# the data is inserted only if the collection is empty (only for testing purposes)
+
 # Inserting data into the database
-if filtered_collection.count_documents({}) == 0:
-    with open(path_to_filtered, "r") as f:
-        filtered_collection.insert_many(eval(f.read()))
+def insert_data(path_to_filedata, collection):
+    with open(path_to_filedata, "r") as f:
+        collection.insert_many(eval(f.read()))
 
-if unfiltered_collection.count_documents({}) == 0:
-    with open(path_to_unfiltered, "r") as f:
-        unfiltered_collection.insert_many(eval(f.read()))
 
-if commits_collection.count_documents({}) == 0:
-    with open(path_to_valid, "r") as f:
-        commits_collection.insert_many(eval(f.read()))
-
-if org_proj_collection.count_documents({}) == 0:
-    with open(path_to_org_proj, "r") as f:
-        org_proj_collection.insert_many(eval(f.read()))
-
-print(filtered_collection.find_one())
+if __name__ == "__main__":
+    import_data()
