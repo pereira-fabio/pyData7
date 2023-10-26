569387c9c9d89f94fdae713e49d4e05d7de51277
pereira-fabio <fabio.pereira.002@student.uni.lu>
2023-10-26 15:28:06 +0200

    created a "script" that is only used for JSON file creation, will save 15 lines of code

--- pydata7/links_analysis/valid_link.py
+++ pydata7/links_analysis/valid_link.py
@@ -5,9 +5,11 @@ import json
 import os
 import datetime
 from pydata7.links_analysis.link_filter import data_filtering
+from pydata7.scripts.json_file_generation import json_file_generation
 
-# Path to the filtered data
-save_path = "../data/"
+
+# # Path to the filtered data
+# save_path = "../data/"
 
 # Path to the data from the data_generator
 path_to_json = data_filtering()
@@ -44,40 +46,46 @@ def is_valid():
     with open(has_commit(), "r") as f:
         valid_commit_links = json.load(f)
 
+    # with open("../data/contains_commit_2023-10-25_15-00-34.json", "r") as f:
+    #     valid_commit_links = json.load(f)
+
     for item in valid_commit_links:
         parts = item["url"].split("/")
+        print(parts)
         repository = "https://" + parts[2] + "/" + parts[3] + "/" + parts[4]
         # If temp is empty, add the first repository
         if temp == []:
             temp.append(repository)
+            print(len(temp))
         # If the repository is not in temp, add it and check if the link is valid
         if repository not in temp:
             temp.append(repository)
-            try:
-                response = requests.head(repository)
-                if response.status_code == 200:
-                    valid_links.append(item)
-            except requests.exceptions.RequestException as err:
-                raise SystemExit(err)
-    return json_file_generation(valid_links, "valid_links")
+            # try:
+            #     response = requests.head(repository)
+            #     if response.status_code == 200:
+            #         valid_links.append(item)
+            #         print(len(valid_links))
+            # except requests.exceptions.RequestException as err:
+            #     raise SystemExit(err)
+    return json_file_generation(temp, "temp")
 
 
-def json_file_generation(array, name):
-    # Gets the current date and time as a string. Year - Month - Day _ Hour - Minute - Second
-    current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-
-    # Saves the data into a json file with the current date and time
-    file_name = f"{name}_{current_time}.json"
-
-    # Creates the path to save the json file
-    file_path = os.path.join(save_path, file_name)
-
-    # Writes the data into a json file
-    with open(file_path, "w") as json_file:
-        json.dump(array, json_file)
-
-    # returns the path to the json file, so it can be used in the next function
-    return file_path
+# def json_file_generation(array, name):
+#     # Gets the current date and time as a string. Year - Month - Day _ Hour - Minute - Second
+#     current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+#
+#     # Saves the data into a json file with the current date and time
+#     file_name = f"{name}_{current_time}.json"
+#
+#     # Creates the path to save the json file
+#     file_path = os.path.join(save_path, file_name)
+#
+#     # Writes the data into a json file
+#     with open(file_path, "w") as json_file:
+#         json.dump(array, json_file)
+#
+#     # returns the path to the json file, so it can be used in the next function
+#     return file_path
 
 
 if __name__ == "__main__":
---
+++ pydata7/scripts/json_file_generation.py
@@ -0,0 +1,23 @@
+import datetime
+import json
+import os
+
+save_path = "../data/"
+
+
+def json_file_generation(array, name):
+    # Gets the current date and time as a string. Year - Month - Day _ Hour - Minute - Second
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+    # Saves the data into a json file with the current date and time
+    file_name = f"{name}_{current_time}.json"
+
+    # Creates the path to save the json file
+    file_path = os.path.join(save_path, file_name)
+
+    # Writes the data into a json file
+    with open(file_path, "w") as json_file:
+        json.dump(array, json_file)
+
+    # returns the path to the json file, so it can be used in the next function
+    return file_path
