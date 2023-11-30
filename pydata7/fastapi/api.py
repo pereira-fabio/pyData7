48cf842663a1588d9e7bf1ac948e3e113ea10ab8
pereira-fabio <fabio.pereira.002@student.uni.lu>
2023-11-30 14:35:47 +0100

    added new endpoints

--- pydata7/fastapi/api.py
+++ pydata7/fastapi/api.py
@@ -20,9 +20,8 @@ def get_collection(collection_name: str, db=Depends(get_database)):
     return db[collection_name]
 
 
-@app.get("/db")
-def coll():
-    return get_database()
+def get_diff_collection(db=Depends(get_database)):
+    return db["commit_diff"]
 
 
 @app.get("/")
@@ -30,21 +29,50 @@ def read_root():
     return {"message": "Hello,FastAPI"}
 
 
-@app.get("/CVE")
+@app.get("/collections")
+def get_collections(db=Depends(get_database)):
+    names_list = []
+    collections_list = db.list_collection_names()
+    print("Available Collections:")
+    for names in collections_list:
+        names_list.append(names)
+        print(names)
+    return names_list
+
+
+@app.get("/cve")
 def list_items(db=Depends(get_collection)):
-    items = list(db.find({}, {"_id": 0, "cve_id": 1}))
-    # print(items)
+    items = list(db.find({}, {"_id": 0, "cve_id": 1, "cvss": 1}).sort("cve_id"))
     return items
 
 
-@app.get("/CVE/{cve_id}")
+@app.get("/cve/{cve_id}")
 def read_item(cve_id: str, db=Depends(get_collection)):
-    result = list(db.find({"cve_id": cve_id}, {"_id": 0, "cve_id": 1, "url": 1, "cvss": 1}))
-    print(result)
+    result = list(db.find({"cve_id": cve_id},
+                          {"_id": 0, "cve_id": 1, "url": 1, "published": 1, "lastModified": 1, "cvss": 1, "patch": 1}))
     if result:
         return result
     else:
-        raise HTTPException(status_code=404, detail="CVE not found")
+        raise HTTPException(status_code=404, detail="CVE not found.")
+
+
+@app.get("/cvss/{cvss}")
+def read_item(cvss: float, db=Depends(get_collection)):
+    result = list(db.find({"cvss": cvss},
+                          {"_id": 0, "cve_id": 1, "url": 1, "published": 1, "lastModified": 1, "cvss": 1, "patch": 1}))
+    if result:
+        return result
+    else:
+        raise HTTPException(status_code=404, detail="CVSS not found.")
+
+
+@app.get("/cve/diff/{cve_id}")
+def read_item(cve_id: str, db=Depends(get_diff_collection)):
+    result = list(db.find({"cve_id": cve_id}, {"_id": 0, "commit_info": 1}))
+    if result:
+        return result
+    else:
+        raise HTTPException(status_code=404, detail="CVE not found.")
 
 
 if __name__ == "__main__":
