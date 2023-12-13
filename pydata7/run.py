77f2dfeec038c6797efa84505d9a47e0e41f3c88
pereira-fabio <fabio.pereira.002@student.uni.lu>
2023-12-13 15:47:53 +0100

    added a connection func

--- pydata7/run.py
+++ pydata7/run.py
@@ -1,8 +1,9 @@
 from pydata7.scripts.repo_downloader import foo
-from pydata7.database_manager.connect_to_db import get_db
+from pydata7.database_manager.connect_to_db import get_db, connection
 
 if __name__ == "__main__":
     # establishing the mongo connection
+    connection()
     get_db()
 
     # download the diffs and other content
