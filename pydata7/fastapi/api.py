19bd384fd8beec87f80bfc34f0b719754fdc4c3e
pereira-fabio <fabio.pereira.002@student.uni.lu>
2023-12-13 01:49:16 +0100

    added comments

--- pydata7/fastapi/api.py
+++ pydata7/fastapi/api.py
@@ -13,15 +13,18 @@ def get_collection(collection_name: str, db=Depends(get_database)):
     return db[collection_name]
 
 
+# Returns the collection containing the diff
 def get_diff_collection(db=Depends(get_database)):
     return db["commit_diff"]
 
 
+# Base URL
 @app.get("/")
 def read_root():
-    return {"message": "Hello,FastAPI"}
+    return {"message": "Hello,PyData7"}
 
 
+# Lists every collection in the client pydata7
 @app.get("/collections")
 def get_collections(db=Depends(get_database)):
     names_list = []
@@ -33,12 +36,14 @@ def get_collections(db=Depends(get_database)):
     return names_list
 
 
+# returns all cve with the corresponding cvss
 @app.get("/cve")
 def list_items(db=Depends(get_collection)):
     items = list(db.find({}, {"_id": 0, "cve_id": 1, "cvss": 1}).sort("cve_id"))
     return items
 
 
+# can take a cve id as input
 @app.get("/cve/{cve_id}")
 def read_item(cve_id: str, db=Depends(get_collection)):
     result = list(db.find({"cve_id": cve_id},
@@ -49,6 +54,7 @@ def read_item(cve_id: str, db=Depends(get_collection)):
         raise HTTPException(status_code=404, detail="CVE not found.")
 
 
+# can take cvss as input
 @app.get("/cvss/{cvss}")
 def read_item(cvss: float, db=Depends(get_collection)):
     result = list(db.find({"cvss": cvss},
@@ -59,6 +65,7 @@ def read_item(cvss: float, db=Depends(get_collection)):
         raise HTTPException(status_code=404, detail="CVSS not found.")
 
 
+# returns the given diff from a specific cve id
 @app.get("/cve/diff/{cve_id}")
 def read_item(cve_id: str, db=Depends(get_diff_collection)):
     result = list(db.find({"cve_id": cve_id}, {"_id": 0, "commit_info": 1}))
@@ -69,6 +76,7 @@ def read_item(cve_id: str, db=Depends(get_diff_collection)):
 
 
 if __name__ == "__main__":
+    # server which fastapi runs on and can be connected to
     import uvicorn
 
     uvicorn.run(app, host="localhost", port=8000)
--- pydata7/run.py
+++ pydata7/run.py
@@ -1,6 +1,9 @@
 from pydata7.scripts.repo_downloader import foo
-from pydata7.database_manager.import_data import import_data
+from pydata7.database_manager.connect_to_db import get_db
 
 if __name__ == "__main__":
-    # foo()
-    import_data()
+    # establishing the mongo connection
+    get_db()
+
+    # download the diffs and other content
+    foo()
