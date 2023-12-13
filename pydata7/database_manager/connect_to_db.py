e8df99111abfb3f59bd5d3efd8d163a9f3866052
pereira-fabio <fabio.pereira.002@student.uni.lu>
2023-12-13 15:49:59 +0100

    implemented dotenv to connect_to_db.py

--- pydata7/database_manager/connect_to_db.py
+++ pydata7/database_manager/connect_to_db.py
@@ -1,14 +1,24 @@
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # I can only connect to a database which is not running on docker container
 # Connecting to the database
-hostname = "localhost"
-port = 27017
-user = "root"
-password = "example"
+hostname = os.getenv("HOST_NAME")
+port = os.getenv("PORT")
+user = os.getenv("USER_MONGO")
+password = os.getenv("PASSWORD_MONGO")
 client = MongoClient(hostname, port, username=user, password=password)
 # client = MongoClient("mongodb://localhost:27017/")
 mydb = client["pydata7"]  # database name
+
+
+def connection():
+    if client.server_info():
+        print("Connection established:", client.address)
+    else:
+        print("Connection not established:", client.address)
 
 
 def get_db():
