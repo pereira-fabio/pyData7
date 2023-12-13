74419a221e1c3ccb104530217a1f88d2f55243db
pereira-fabio <fabio.pereira.002@student.uni.lu>
2023-12-13 15:58:21 +0100

    added run_uvicorn to run.py

--- pydata7/run.py
+++ pydata7/run.py
@@ -1,5 +1,6 @@
 from pydata7.scripts.repo_downloader import foo
 from pydata7.database_manager.connect_to_db import get_db, connection
+from pydata7.fastapi.api import run_uvicorn
 
 if __name__ == "__main__":
     # establishing the mongo connection
@@ -8,3 +9,6 @@ if __name__ == "__main__":
 
     # download the diffs and other content
     foo()
+
+    # start FastAPI server
+    run_uvicorn()
