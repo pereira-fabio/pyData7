a033ab71292818bcf8b22c1a93d5b9264fb40fdc
pereira-fabio <fabio.pereira.002@student.uni.lu>
2023-12-19 19:51:04 +0100

    updated the run

--- pydata7/run.py
+++ pydata7/run.py
@@ -1,14 +1,12 @@
-#from pydata7.scripts.repo_downloader import foo
-from pydata7.database_manager.connect_to_db import get_db, connection
-#from pydata7.fastapi.api import run_uvicorn
+from pydata7.scripts.repo_downloader import foo
+# from pydata7.database_manager.connect_to_db import get_db, get_client, connection, close_connection
+from pydata7.fastapi.api import run_uvicorn
 
 if __name__ == "__main__":
-    # establishing the mongo connection
-    connection()
-    #get_db()
-
     # download the diffs and other content
-    #foo()
+    print("Getting diff:")
+    foo()
 
     # start FastAPI server
-    #run_uvicorn()
+    print("Running API server:")
+    run_uvicorn()
